78619f641157957c3c32e6cd865e4cd16d6951be
spogmayshirzai <s.shirzai95@gmail.com>
2023-09-06 03:58:29 -0400

    initail project change

---
+++ src/tek/git/FirstClass.java
@@ -0,0 +1,15 @@
+package tek.git;
+
+public class FirstClass {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		
+		//what should I do to push this project to git hub repository or step by step to push this project to a remote repository?
+				
+				//What is my first step before doing it ?
+		// first step a new create new repository in github
+		
+	}
+
+}
