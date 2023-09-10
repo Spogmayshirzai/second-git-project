599e98d9738100b737044ff2cbd73a4de10391a2
spogmayshirzai <s.shirzai95@gmail.com>
2023-09-10 00:21:24 -0400

    New Task2

--- src/tek/git/tasks/Task1.java
+++ src/tek/git/tasks/Task1.java
@@ -2,4 +2,7 @@ package tek.git.tasks;
 
 public class Task1 {
 
+	public static void main(String[] args) {
+		System.out.println("This is Task 1 from newTask1 branch");
+	}
 }
---
+++ src/tek/git/tasks/Tast2.java
@@ -0,0 +1,10 @@
+package tek.git.tasks;
+
+public class Tast2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Task 2 from newTask2 branch");
+	}
+
+}
