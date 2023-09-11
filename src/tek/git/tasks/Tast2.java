9daf5c279fdca3fc8de234cfec2c44f20ae601d6
spogmayshirzai <s.shirzai95@gmail.com>
2023-09-11 15:58:42 -0400

    updating existing class

--- src/tek/git/tasks/Tast2.java
+++ src/tek/git/tasks/Tast2.java
@@ -5,6 +5,8 @@ public class Tast2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is Task 2 from newTask2 branch");
+		
+		System.out.println("This is update from new branch");
 	}
 
 }
